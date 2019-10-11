9114bb9006f264dc82ca2652376ebabf9c3fbc08
Hemil Panchiwala <hpanchiwala@cs.iitr.ac.in>
2019-10-11 22:18:46 +0530

    Added the tabs for sports and technology news

--- Tabs/Tab2.js
+++ Tabs/Tab2.js
@@ -1,14 +1,81 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, ActivityIndicator, StyleSheet, ToastAndroid, Alert } from 'react-native';
+import { Container, Header, Content, List, ListItem, Thumbnail, Left,  Body, Right, Button } from 'native-base';
+import {getArticles} from '../service/news'
+import DataItem from '../components/dataItem'
+import Modal from '../components/modal'
 
-class Tab2 extends Component {
+export default class Tab2 extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            isLoading: true,
+            data: null,
+            setModalVisibility: false,
+            modalData: {}
+        }
+    }
+
+    handleOnClickView = (article) => {
+        this.setState({
+            setModalVisibility: true,
+            modalData: article
+        })
+    }
+
+    handleOnClose = () => {
+        this.setState({
+            setModalVisibility: false,
+            modalData: {}
+        })  
+    }
+
+    componentDidMount() {
+        getArticles('sports').then(data => {
+            this.setState({
+                isLoading: false,
+                data: data
+            })
+        }, error => {
+            Alert.alert('error', 'Something went wrong!!!')
+        })
+    }
+
   render() {
+
+    let view = this.state.isLoading ? (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <ActivityIndicator animating={this.state.isLoading} color="#00f0ff" />
+          <Text style={{marginTop: 10}} children="Please Wait.." />
+        </View>
+      ) : (
+        <List
+          dataArray={this.state.data}
+          renderRow={(item) => {
+              return (
+                <DataItem key={Math.random().toString(36).substring(7)} onPress={this.handleOnClickView} data={item} />
+              )
+          }} />
+      )
+
     return (
-      <View>
-        <Text> Tab2 </Text>
-      </View>
+      <Container>
+          <Content>
+            {view}
+          </Content>
+          <Modal 
+            showModal={this.state.setModalVisibility}
+            articleData={this.state.modalData}
+            onClose={this.handleOnClose} />
+      </Container>
     );
   }
 }
 
-export default Tab2;
+const styles = StyleSheet.create({
+    header: {
+        backgroundColor: '#009387'
+    }
+})
--- service/news.js
+++ service/news.js
@@ -1,12 +1,17 @@
-import {url, country_code, category, apiKey} from '../config/rest_config';
+import {url, country_code, apiKey} from '../config/rest_config';
+import {ToastAndroid} from 'react-native'
 
-export async function getArticles() {
+export async function getArticles(category='general') {
 
     try{
-        let article = await fetch(`${url}?country=${country_code}&
-        category=${category}&apiKey=${apiKey}`)
+        let article = await fetch(`${url}?country=${country_code}&category=${category}`,{
+            headers: {
+                'X-API-KEY': apiKey
+            }
+        })
 
         let result = await article.json()
+        console.log(result.articles)
 
         return result.articles;
     }catch{
